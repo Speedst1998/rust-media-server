03841106ef9934324789bc5b839d69ad6b763620
Tahar Mustapha <mtahar1998@gmail.com>
2023-05-28 18:23:50 -0400

    removed redundant return

--- src/clients/public_ip.rs
+++ src/clients/public_ip.rs
@@ -1,9 +1,8 @@
 pub async fn get_public_ip() -> String {
-    let result = reqwest::get("http://whatismyip.akamai.com/")
+    reqwest::get("http://whatismyip.akamai.com/")
         .await
         .unwrap()
         .text()
         .await
-        .unwrap();
-    result
+        .unwrap()
 }
